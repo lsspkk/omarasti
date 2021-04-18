c6d21f181fc85d855daf4938de1854c3c52b4697
lsspkk <lsspkk@gmail.com>
2021-04-18 20:27:46 +0300

    added marker numbers

--- omarasti/components/TrackMarker.js
+++ omarasti/components/TrackMarker.js
@@ -9,7 +9,7 @@ import { Button } from './Buttons'
 
 export const MARKER_SIZE = 50
 
-const CustomRastiMarker = () => {
+const RunTrackMarker = () => {
   return (
     <svg style={{ position: 'relative', marginLeft: "-20px", marginTop: "-20px" }}
       width={MARKER_SIZE} height={MARKER_SIZE} viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
@@ -20,13 +20,26 @@ const CustomRastiMarker = () => {
   )
 }
 
-export const TrackMarker = ({ marker, published }) => {
+const StartTrackMarker = () => {
+  return (
+    <svg style={{ position: 'relative', marginLeft: "-20px", marginTop: "-20px" }}
+      width={MARKER_SIZE} height={MARKER_SIZE} viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <circle cx={MARKER_SIZE} cy={MARKER_SIZE} r='45' stroke='#FA923B' strokeWidth='10' />
+      <circle cx={MARKER_SIZE} cy={MARKER_SIZE} r='40' stroke='rgba(0,0,0,0.5)' strokeWidth='1' />
+      <circle cx={MARKER_SIZE} cy={MARKER_SIZE} r={MARKER_SIZE} stroke='rgba(0,0,0,0.5)' strokeWidth='1' />
+    </svg>
+  )
+}
+
+
+export const TrackMarker = ({ marker, published, index, isLastMarker }) => {
   const [track, setTrack] = useRecoilState(trackState)
   const [mode] = useRecoilState(designModeState)
   const [description, setDescription] = useState(marker.description)
-  const iconMarkup = renderToStaticMarkup(<CustomRastiMarker />)
+  const iconMarkup = renderToStaticMarkup(<RunTrackMarker />)
+  const number= (index > 0 && !isLastMarker) ? index : ''
   const customMarkerIcon = divIcon({
-    html: iconMarkup
+    html: `<span class="track-marker-number">${number}</span>${iconMarkup}`
   })
 
   const onClick = () => {
