ffc132ced3ba81f86e88aa7793e9c4f581b4c07a
lsspkk <lsspkk@gmail.com>
2025-09-07 00:12:53 +0300

    feat: add authentication options with Google provider integration

---
+++ omarasti/auth.js
@@ -0,0 +1,21 @@
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
